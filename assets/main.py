07f7fbfb4f035cba4b9d025d5985dfc2d62260a1
nav-r-c <abhinav.30.kr@gmail.com>
2022-12-25 14:32:52 +0530

    Added Dynamic Wallpaper Path Support

--- assets/main.py
+++ assets/main.py
@@ -4,16 +4,20 @@ import numpy as np
 class WallpaperChanger:
     def __init__(self) -> None:
         print(__file__)
+        self.getWallpaperPath()
         self.getRandomWallpaper()
         self.changeWallpaper(self.wallpaper)
+
+    def getWallpaperPath(self) -> None:
+        self.wallpaperPath = input("Enter Path to Wallpapers: ")
     
     def getRandomWallpaper(self) -> None:
-        files = os.listdir("/home/nav-r/Pictures/Wallpapers")
+        files = os.listdir(f"{self.wallpaperPath}")
         self.wallpaper = np.random.choice(files)
         
     def changeWallpaper(self, wallpaper) -> None: 
-        os.system(f"gsettings set org.gnome.desktop.background picture-uri-dark \"file:///home/nav-r/Pictures/Wallpapers/{wallpaper}\"")
-        os.system(f"gsettings set org.gnome.desktop.background picture-uri \"file:///home/nav-r/Pictures/Wallpapers/{wallpaper}\"")
+        os.system(f"gsettings set org.gnome.desktop.background picture-uri-dark \"file://{self.wallpaperPath}/{wallpaper}\"")
+        os.system(f"gsettings set org.gnome.desktop.background picture-uri \"file://{self.wallpaperPath}/{wallpaper}\"")
         print(f"Changed Wallpaper to: {wallpaper}")
 
 if __name__ == "__main__":
