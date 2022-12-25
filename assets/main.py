01d756c8c184a661644bca589d0a343bbcf14f8f
nav-r-c <abhinav.30.kr@gmail.com>
2022-12-25 14:38:22 +0530

    Added Cache Support

--- assets/main.py
+++ assets/main.py
@@ -1,15 +1,20 @@
 import os
 import numpy as np
+import pickle
 
 class WallpaperChanger:
     def __init__(self) -> None:
-        print(__file__)
         self.getWallpaperPath()
         self.getRandomWallpaper()
         self.changeWallpaper(self.wallpaper)
+        self.storeWallpaperPath()
 
     def getWallpaperPath(self) -> None:
-        self.wallpaperPath = input("Enter Path to Wallpapers: ")
+        if ("path.bin" not in os.listdir(__file__.rstrip("main.py"))):
+            self.wallpaperPath = input("Enter Path to Wallpapers: ")
+        else:
+            file = open("path.bin", "rb")
+            self.wallpaperPath = pickle.load(file)
     
     def getRandomWallpaper(self) -> None:
         files = os.listdir(f"{self.wallpaperPath}")
@@ -19,6 +24,11 @@ class WallpaperChanger:
         os.system(f"gsettings set org.gnome.desktop.background picture-uri-dark \"file://{self.wallpaperPath}/{wallpaper}\"")
         os.system(f"gsettings set org.gnome.desktop.background picture-uri \"file://{self.wallpaperPath}/{wallpaper}\"")
         print(f"Changed Wallpaper to: {wallpaper}")
+    
+    def storeWallpaperPath(self) -> None:
+        with open("path.bin", "wb") as file:
+            pickle.dump(self.wallpaperPath, file)
+        
 
 if __name__ == "__main__":
     w = WallpaperChanger()
